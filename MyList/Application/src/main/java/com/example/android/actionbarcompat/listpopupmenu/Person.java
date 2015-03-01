3c6e423d0b3017ba864e25a16894bc3593b50641
Karrie Cheng <karriecheng@tamu.edu>
2015-03-01 01:40:37 -0600

    List
    Adds list view with Person name but no image

---
+++ MyList/Application/src/main/java/com/example/android/actionbarcompat/listpopupmenu/Person.java
@@ -0,0 +1,13 @@
+package com.example.android.actionbarcompat.listpopupmenu;
+
+/**
+ * Created by karriecheng on 3/1/15.
+ */
+public class Person {
+    public String name;
+    public String img;
+    public Person(String n, String i){
+        name = n;
+        img = i;
+    }
+}
